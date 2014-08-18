02c99ee03b73d9eec46f223cafcc8d4f0d54873c
daniel <Daniel Reid>
2014-08-17 19:44:33 -0500

    first

---
+++ figgering/hello.c
@@ -0,0 +1,13 @@
+#define F_CPU 1000000
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main() {
+	DDRB = _BV(0);
+	while (1) {
+		_delay_ms(200);
+		PORTB = _BV(0);
+		_delay_ms(200);
+		PORTB = 0;
+	}
+}
